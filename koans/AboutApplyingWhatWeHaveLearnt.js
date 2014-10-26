1f23002a87e704d3952695d6a28ac69795a7c010
Scott Rice <srice@infopluswms.com>
2014-10-25 20:09:00 -0500

    Use chaining range() and reduce() to fix tests.

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -60,9 +60,19 @@ describe("About Applying What We Have Learnt", function() {
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
 
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
+     /* try chaining range() and reduce() */
+     var sum = _(1000).chain()
+                 .range()
+                 .reduce(function(sum, i) {
+                    if (i % 3 === 0 || i % 5 === 0) {
+                       sum += i;
+                    }
 
-    expect(233168).toBe(FILL_ME_IN);
+                    return sum;
+                 })
+                 .value();
+
+    expect(233168).toBe(sum);
   });
 
   /*********************************************************************************/
